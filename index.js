1d4ef53239b19e644c8b1e2f80c93a633b2f574e
erin johnson <erin.nicole.johnson@gmail.com>
2019-01-26 15:06:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function increaseRankBy(n) {
 
     for (let i = 0; i < lis.length; i++) {
     //  lis[i].innerHTML = (i + increaseRank).toString();
-      lis[i].innerHTML = parseInt(lis[i].innerHTML + n);
+      lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
     }
 
 };
