51fd8095d02ce8f7012e7e83b3ca2909f04ae9f0
erin johnson <erin.nicole.johnson@gmail.com>
2019-01-26 14:58:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function getFirstSelector(selector) {  //PASSING
+  //return the first element that matches
+  var mySelector = document.querySelector(selector);
+  return mySelector;
+};
 
+
+function nestedTarget() { //PASSING
+  //pulls a .target out of #nested
+  var myTarget = document.querySelector('#nested .target')
+  return myTarget;
+}
+
+function increaseRankBy(n) {
+  //increases the rank in all the .ranked-list by n
+  // analyse string, returns a number
+  var increaseRank = parseInt(n); //set a variable that parses the input
+  var foo = document.querySelectorAll('ul.ranked-list li'); //get the li's and set them as a variable
+    for (let i = 0; i < foo.length; i++) {
+      foo[i].innerHTML = (i + increaseRank).toString();
+    }
+
+
+};
+
+function deepestChild() {
+  var meep = document.querySelector('main#app div#grand-node div div div div');
+
+  return meep.innerHTML;
+}
