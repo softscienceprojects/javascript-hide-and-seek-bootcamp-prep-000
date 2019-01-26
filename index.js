3b2cb1fca2b6f65cd3d2a90839a2343a7927d797
erin johnson <erin.nicole.johnson@gmail.com>
2019-01-26 15:00:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ function nestedTarget() { //PASSING
 
 function increaseRankBy(n) {
   //increases the rank in all the .ranked-list by n
-  // analyse string, returns a number
-  var increaseRank = parseInt(n); //set a variable that parses the input
-  var foo = document.querySelectorAll('ul.ranked-list li'); //get the li's and set them as a variable
-    for (let i = 0; i < foo.length; i++) {
-      foo[i].innerHTML = (i + increaseRank).toString();
-    }
+  const lis = document
+    .getElementById('app')
+    .querySelectorAll('ul.ranked-list li');
 
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + 1).toString();
+  }
 
 };
 
